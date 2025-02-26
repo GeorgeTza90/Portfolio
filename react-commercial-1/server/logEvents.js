31298f3a19b041ef93a0b4be4bca57ec6d7a1170
George Tzachristas <gtzahristas@hotmail.com>
2025-02-26 20:16:54 +0200

    Update

--- react-commercial-1/server/logEvents.js
+++ react-commercial-1/server/logEvents.js
@@ -1,27 +1,23 @@
 const { format } = require('date-fns');
 const { v4: uuid } = require('uuid');
-const fsPromises = require('fs').promises;
-const path = require('path');
 
-const LogDir = path.join(__dirname, 'temp'); 
-const LogPath = path.join(LogDir, 'logEvents.txt'); 
+const fs = require('fs');
+const fsPromises = require('fs').promises;
+const path = require('path')
+
+const LogPath = path.join(__dirname, 'temp', 'logEvents.txt');
+
 
 const logEvents = async (message, logName) => {
     const dateTime = `${format(new Date(), 'MM-dd-yyyy\tHH:mm:ss')}`;
     const logItem = `${dateTime}\t${uuid()}\t${message}\t${logName}\n`;
     console.log(logItem);
-
     try {
-        const fileExists = await fsPromises.access(LogPath)
-            .then(() => true)
-            .catch(() => false);
-            
-        if (!fileExists) {
-            await fsPromises.writeFile(LogPath, ''); 
+        if(!fs.existsSync( LogPath )) {            
+            await fsPromises.writeFile(LogPath, '');
         }
-
-        await fsPromises.appendFile(LogPath, logItem);
-    } catch (err) {
+        await fsPromises.appendFile( LogPath , logItem);
+    } catch(err) {
         console.log(err);
     }
 }
