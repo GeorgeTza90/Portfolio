1a10537514a7ecad6d0cdce970ac40eed8bea21c
George Tzachristas <gtzahristas@hotmail.com>
2025-02-27 14:52:26 +0200

    Update

--- react-commercial-1/server/controllers/aboutController.js
+++ react-commercial-1/server/controllers/aboutController.js
@@ -3,7 +3,7 @@ const LogEvents = require('../logEvents');
 
 exports.getAbout = async (req, res) => {
   try {
-    res.json({ heading: "Sign In" });
+    res.json({ heading: "About Us" });
   } catch (error) {
     console.error("Error on About Page:", error);
     res.status(500).json({ message: "Internal Server Error" });  }
