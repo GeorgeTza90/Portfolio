374fd406d57b9d02bb608c57a4b9c27798f8d57b
George Tzachristas <gtzahristas@hotmail.com>
2025-03-27 14:15:33 +0200

    Build Update

--- react-commercial-1/server/db.js
+++ react-commercial-1/server/db.js
@@ -1,11 +1,11 @@
 const mysql = require("mysql2");
 
 const db = mysql.createConnection({
-  host: process.env.MYSQL_HOST,            // Internal host provided by Railway
-  user: process.env.MYSQL_USER,            // MySQL user (often 'root' or the user you created)
-  password: process.env.MYSQL_PASSWORD,    // MySQL password
-  database: process.env.MYSQL_DATABASE,    // Database name
-  port: process.env.MYSQL_PORT || 3306     // Port (defaults to 3306, but use MYSQL_PORT if set)
+  host: process.env.MYSQL_HOST,            
+  user: process.env.MYSQL_USER,            
+  password: process.env.MYSQL_PASSWORD,    
+  database: process.env.MYSQL_DATABASE,    
+  port: process.env.MYSQL_PORT,       
 });
 
 
