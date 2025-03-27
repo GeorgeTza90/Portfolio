68cc771b0d1eff569193db579df62cd81a5c17be
George Tzachristas <gtzahristas@hotmail.com>
2025-03-27 18:03:20 +0200

    Update frontend build

--- react-commercial-1/client/src/services/GetService.js
+++ react-commercial-1/client/src/services/GetService.js
@@ -1,11 +1,11 @@
 import axios from "axios";
-const API_URL = process.env.REACT_APP_API_URL || 'https://icvacations.up.railway.app';
+// const API_URL = process.env.REACT_APP_API_URL || 'https://icvacations.up.railway.app';
 
 
 class GetService {
   constructor() {
     this.api = axios.create({
-      baseURL: `${API_URL}/api`, 
+      baseURL: `https://icvacations.up.railway.app/api`, 
     });
   }
 
