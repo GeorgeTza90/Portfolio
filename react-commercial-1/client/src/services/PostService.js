f4f8a9b15f6541d433b635f2866e76751ea987a2
George Tzachristas <gtzahristas@hotmail.com>
2025-03-27 19:02:31 +0200

    Update frontend build

--- react-commercial-1/client/src/services/PostService.js
+++ react-commercial-1/client/src/services/PostService.js
@@ -1,5 +1,6 @@
 import axios from "axios";
-const API_URL = process.env.REACT_APP_API_URL || 'https://icvacations.up.railway.app';
+// const API_URL = process.env.REACT_APP_API_URL || 'https://icvacations.up.railway.app';
+const API_URL = 'https://icvacations.up.railway.app';
 
 
 class PostService {
