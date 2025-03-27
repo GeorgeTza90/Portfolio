2def509302a4b13953c8d6f91a3bb0b37050e7cc
George Tzachristas <gtzahristas@hotmail.com>
2025-03-27 16:24:14 +0200

    Update frontend build

--- react-commercial-1/server/knexfile.js
+++ react-commercial-1/server/knexfile.js
@@ -5,11 +5,11 @@ module.exports = {
   development: {
     client: 'mysql2',
     connection: {
-      host: process.env.DB_HOST || '127.0.0.1',
-      user: process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_NAME,
-      port: process.env.PORT || 3306
+      host: process.env.MYSQL_HOST,            
+      user: process.env.MYSQL_USER,            
+      password: process.env.MYSQL_PASSWORD,    
+      database: process.env.MYSQL_DATABASE,    
+      port: process.env.MYSQL_PORT,  
     },
     migrations: {
       directory: './migrations'
@@ -23,11 +23,11 @@ module.exports = {
   production: {
     client: 'mysql2',
     connection: {
-      host: process.env.DB_HOST ,
-      user: process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_NAME,
-      port: process.env.PORT || 5000
+      host: process.env.MYSQL_HOST,            
+      user: process.env.MYSQL_USER,            
+      password: process.env.MYSQL_PASSWORD,    
+      database: process.env.MYSQL_DATABASE,    
+      port: process.env.MYSQL_PORT,  
     },
     migrations: {
       directory: './migrations'
