e5551ad68c1149b164ffed34543596864177172e
George Tzachristas <gtzahristas@hotmail.com>
2025-03-29 12:02:20 +0200

    update

--- react-commercial-1/server/db.js
+++ react-commercial-1/server/db.js
@@ -1,25 +1,20 @@
-const mysql = require("mysql2/promise");
+const mysql = require("mysql2");
 
-const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0
+const db = mysql.createConnection({
+  host: process.env.MYSQL_HOST,            
+  user: process.env.MYSQL_USER,            
+  password: process.env.MYSQL_PASSWORD,    
+  database: process.env.MYSQL_DATABASE,    
+  port: process.env.MYSQL_PORT,       
 });
 
-async function checkDbConnection() {
-  try {
-    const connection = await pool.getConnection();
-    console.log("Database connected successfully");
-    connection.release();
-  } catch (error) {
-    console.error("Database connection error:", error);
+
+db.connect((err) => {
+  if (err) {
+    console.error("Database connection failed:", err);
+    return;
   }
-}
+  console.log("Connected to MySQL Database");
+});
 
-checkDbConnection();
-
-module.exports = pool;
+module.exports = db;
