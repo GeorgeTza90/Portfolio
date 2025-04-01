179d30f238f6f08a35ce867dd95114a8f97fbbdb
George Tzachristas <gtzahristas@hotmail.com>
2025-04-01 18:22:23 +0300

    Update

--- react-commercial-1/server/server.js
+++ react-commercial-1/server/server.js
@@ -3,8 +3,7 @@ const cors = require("cors");
 require("dotenv").config();
 const axios = require("axios");
 const app = express();
-
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 
 
 
--- react-commercial-2/server/server.js
+++ react-commercial-2/server/server.js
@@ -3,7 +3,7 @@ const cors = require("cors");
 const fileUpload = require("express-fileupload"); 
 require("dotenv").config();
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 
 // CONTROLLERS
 const homeController = require("./controllers/homeController");
