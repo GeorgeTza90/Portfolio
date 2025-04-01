f526b1166cd06794fc531d71d0a719a904c6ab3b
George Tzachristas <gtzahristas@hotmail.com>
2025-04-01 18:57:37 +0300

    Update

--- react-commercial-1/server/db.js
+++ react-commercial-1/server/db.js
@@ -5,12 +5,14 @@ const pool = mysql.createPool({
   user: process.env.MYSQL_USER,
   password: process.env.MYSQL_PASSWORD,
   database: process.env.MYSQL_DATABASE,
-  port: process.env.MYSQL_PORT,
-  connectTimeout: 30000, 
-  acquireTimeout: 30000,
+  port: process.env.MYSQL_PORT || 3306, 
+  connectTimeout: 30000,  
   waitForConnections: true,
   connectionLimit: 10,
-  queueLimit: 0
+  queueLimit: 0,
+  ssl: {
+    rejectUnauthorized: false, 
+  }
 });
 
 pool.getConnection()
