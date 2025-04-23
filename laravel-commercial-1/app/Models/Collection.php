a9a7a8d2f8211e99079b00e2e36c6c0a10e6d539
George Tzachristas <gtzahristas@hotmail.com>
2025-04-23 18:21:50 +0300

    Laravel Upload

--- laravel-commercial-1/app/Models/Collection.php
+++ laravel-commercial-1/app/Models/Collection.php
@@ -5,6 +5,8 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use mtgsdk\Card;
+use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Facades\Log;
 
 class Collection extends Model
 {
@@ -12,13 +14,20 @@ class Collection extends Model
 
     public $timestamps = false;
 
-
     public static function get($cardIds)
     {
         $collection = [];
 
         foreach ($cardIds as $cardId) {
-            $card = Card::find($cardId);
+            $card = Cache::remember("mtg_card_{$cardId}", now()->addHours(6), function () use ($cardId) {
+                try {
+                    return Card::find($cardId);
+                } catch (\Exception $e) {
+                    Log::error("MTG API error for card {$cardId}: " . $e->getMessage());
+                    return null;
+                }
+            });
+
             if ($card) {
                 $collection[] = $card;
             }
