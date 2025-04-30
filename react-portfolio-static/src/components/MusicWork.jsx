262e8688970ecec187db363bcfb8ddbeb0921391
George Tzachristas <gtzahristas@hotmail.com>
2025-04-30 13:49:56 +0300

    Update

--- react-portfolio-static/src/components/MusicWork.jsx
+++ react-portfolio-static/src/components/MusicWork.jsx
@@ -5,26 +5,26 @@ function MusicWork() {
         <>
             <h1 className={styles.sectionTitle}>Music Work</h1>
             <div className={styles.musicWork}>
-                <a href="https://icvacations.netlify.app/" target="_blank"
+                <a href="https://distrokid.com/hyperfollow/neperia/zenaidas-voice" target="_blank"
                     className={`${styles.musicBox} ${styles.zenaida}`}></a>
-                <a href="https://icvacations.netlify.app/" target="_blank"
+                <a href="https://open.spotify.com/album/0uywQMvMlvIm25Y6AuvIiX?si=W2bMgklWS72_n0PNwzVhSQ" target="_blank"
                     className={`${styles.musicBox} ${styles.happy}`}></a>
-                <a href="https://icvacations.netlify.app/" target="_blank"
+                <a href="https://open.spotify.com/album/2GKXZGt6iB3yeCb9Kgx819?si=4fM1DOVVSzKToXRRfJXG5w" target="_blank"
                     className={`${styles.musicBox} ${styles.p13}`}></a>
-                <a href="https://icvacations.netlify.app/" target="_blank"
+                <a href="https://open.spotify.com/album/0CEjQS9olKwHd7zFxoWWCw?si=SiGUv7KiSmekOaXqLTjuFg" target="_blank"
                     className={`${styles.musicBox} ${styles.vritra}`}></a>
-                <a href="https://icvacations.netlify.app/" target="_blank"
+                <a href="https://open.spotify.com/album/4KwSaACCVsnWI3hhAcDrLd?si=3bqXgAsuSfG0xYOjtvvwXA" target="_blank"
                     className={`${styles.musicBox} ${styles.minokawa}`}></a>
 
-                <a href="https://icvacations.netlify.app/" target="_blank"
+                <a href="https://open.spotify.com/album/0YUI0rMbJLVADbVWgRpYLQ?si=LWIlGAveR2C-1vKvOFKsig" target="_blank"
                     className={`${styles.musicBox} ${styles.scalling}`}></a>
-                <a href="https://icvacations.netlify.app/" target="_blank"
+                <a href="https://open.spotify.com/album/0CChaj3uxMeZc7PVRx79V8?si=4kZMnWGaTa-rBUcCmaBX6Q" target="_blank"
                     className={`${styles.musicBox} ${styles.runner}`}></a>
-                <a href="https://icvacations.netlify.app/" target="_blank"
+                <a href="https://open.spotify.com/album/4tJWD8sk5Bef99fEW8qqhX?si=5mWHoDlmSlaDRahSFV1juQ" target="_blank"
                     className={`${styles.musicBox} ${styles.ptesmata}`}></a>
-                <a href="https://icvacations.netlify.app/" target="_blank"
+                <a href="https://open.spotify.com/album/0M7884IhTxOdTZtBK3CsXh?si=R3jgdlTwRXOzz_KlBla8Ug" target="_blank"
                     className={`${styles.musicBox} ${styles.cybernation}`}></a>
-                <a href="https://icvacations.netlify.app/" target="_blank"
+                <a href="https://open.spotify.com/album/5fviige0btTbQf2hZQ6T8W?si=JkFKXt2fQSSrGSXfsaX0gg" target="_blank"
                     className={`${styles.musicBox} ${styles.drawing}`}></a>
             </div>
             <br />
