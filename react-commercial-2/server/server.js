c456bfcd1388646ba3c7e5185942c71b1ea97665
George Tzachristas <gtzahristas@hotmail.com>
2025-10-16 01:09:31 +0300

    Self Ping Removed - removed axios on Server

--- react-commercial-2/server/server.js
+++ react-commercial-2/server/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const fileUpload = require("express-fileupload"); 
 require("dotenv").config();
-const axios = require("axios");
+// const axios = require("axios");
 const app = express();
 const PORT = process.env.PORT;
 
