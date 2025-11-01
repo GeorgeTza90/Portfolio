363ad9b7ccfe5f907edfd8db02e5bc4153560a83
George Tzachristas <gtzahristas@hotmail.com>
2025-11-01 18:23:00 +0200

    Update

--- eclipse_player/server/utils/sendEmail.js
+++ eclipse_player/server/utils/sendEmail.js
@@ -6,7 +6,7 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 async function sendEmail(to, subject, html) {
   try {
     const data = await resend.emails.send({
-      from: "Eclipse Player <onboarding@resend.dev>",
+      from: "Eclipse Player <onboarding@send.eclipseplayer.com>",
       to,
       subject,
       html,
@@ -14,7 +14,7 @@ async function sendEmail(to, subject, html) {
 
     console.log('Email sent:', data);
   } catch (error) {
-    console.error('Error sending email:', error);
+    console.error('Error sending email:', error.response?.data || error);
     throw error;
   }
 }
