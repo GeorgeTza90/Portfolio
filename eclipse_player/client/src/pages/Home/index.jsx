e4042c2523d0c25975e370ecee02f2a21460ac82
George Tzachristas <gtzahristas@hotmail.com>
2025-12-29 12:19:01 +0200

    Mini Player Update

--- eclipse_player/client/src/pages/Home/index.jsx
+++ eclipse_player/client/src/pages/Home/index.jsx
@@ -1,17 +1,19 @@
 import HomeScreen from "../../components/home/HomeScreen";
 import MiniPlayer from "../../components/player/MiniPlayer";
 import Circle from "../../components/ui/Circle";
+import { useAuth } from "../../contexts/AuthContextWeb";
 import { useIsMobile } from "../../hooks/useIsMobile";
 
 function Home() {
     const isMobile = useIsMobile();
+    const { user, token } = useAuth();
 
     return (<>
         <div id="heading" style={{ display: "flex", justifyContent: "center", width: '100%' }}>
             <img src="/assets/images/HomeLogo.png" style={{ position: 'absolute', width: 90, top: isMobile ? 10 : 60 }} />
             <Circle size={isMobile ? 400 : 1000} top={-isMobile ? -320 : -880} shadowColor="#201f1fff" color2="#0b0b0bff" color1="#1f1e1eff" />
             <HomeScreen />
-            {!isMobile && (<MiniPlayer />)}
+            {!isMobile && user && token && (<MiniPlayer />)}
         </div>
     </>);
 }
--- eclipse_player/client/src/pages/Library/index.jsx
+++ eclipse_player/client/src/pages/Library/index.jsx
@@ -2,16 +2,18 @@ import Circle from "../../components/ui/Circle";
 import LibraryScreen from "../../components/library/LibraryScreen";
 import { useIsMobile } from "../../hooks/useIsMobile";
 import MiniPlayer from "../../components/player/MiniPlayer";
+import { useAuth } from "../../contexts/AuthContextWeb";
 
 function Library() {
     const isMobile = useIsMobile();
+    const { user, token } = useAuth();
 
     return (<>
         <div id="heading" style={{ display: "flex", justifyContent: "center" }}>
             <img src="/assets/images/LibraryLogo.png" style={{ position: 'absolute', width: 120, top: isMobile ? 10 : 55 }} />
             <Circle size={isMobile ? 400 : 1000} top={-isMobile ? -320 : -880} shadowColor="#201f1fff" color2="#0b0b0bff" color1="#1f1e1eff" />
             <LibraryScreen />
-            {!isMobile && (<MiniPlayer />)}
+            {!isMobile && user && token && (<MiniPlayer />)}
         </div>
     </>);
 }
