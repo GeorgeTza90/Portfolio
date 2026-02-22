04df845a2f28f7df7f4e8f2904938f8cd1033eda
George Tzachristas <gtzahristas@hotmail.com>
2026-02-22 14:39:48 +0200

    February Big Update v1.0.2

--- eclipse_player/desktop/Eclipse_Player_Desktop/obj/ARM64/Debug/net8.0-windows10.0.19041.0/win-arm64/Eclipse_Player_Desktop.AssemblyInfo.cs
+++ eclipse_player/desktop/Eclipse_Player_Desktop/obj/ARM64/Debug/net8.0-windows10.0.19041.0/win-arm64/Eclipse_Player_Desktop.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Eclipse_Player_Desktop")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+587a350fb127d3c9900206ab178d236003993643")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d09928f30daff6ac74cb98795f372eef32b75142")]
 [assembly: System.Reflection.AssemblyProductAttribute("Eclipse_Player_Desktop")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Eclipse_Player_Desktop")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
