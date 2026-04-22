bce09a2e958440121f4092720424faa440a17611
George Tzachristas <gtzahristas@hotmail.com>
2026-04-23 01:39:23 +0300

    User Settings Bug Fix v1

--- eclipse_player/desktop/Eclipse_Player_Desktop/obj/ARM64/Debug/net8.0-windows10.0.19041.0/win-arm64/Eclipse_Player_Desktop.AssemblyInfo.cs
+++ eclipse_player/desktop/Eclipse_Player_Desktop/obj/ARM64/Debug/net8.0-windows10.0.19041.0/win-arm64/Eclipse_Player_Desktop.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Eclipse_Player_Desktop")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6308e303f2df681b7dd2fc3e3da8317a1bb1732e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4fa3963697b1e7b6ad7f49932951cc39462b11aa")]
 [assembly: System.Reflection.AssemblyProductAttribute("Eclipse_Player_Desktop")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Eclipse_Player_Desktop")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
