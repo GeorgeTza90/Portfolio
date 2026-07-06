555d0ecd4b7f5c3d7245318e4e7d4e41283f53b6
George Tzachristas <gtzahristas@hotmail.com>
2026-07-06 15:46:11 +0300

    Portfolio Update v3

--- angular-portfolio-static/src/app/music-work/music-work.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-music-work',
-  standalone: true,
-  templateUrl: './music-work.component.html',
-  styleUrls: ['./music-work.component.css']
-})
-export class MusicWorkComponent {}
