eba6c645771452bfffefac70e88305202444e943
George Tzachristas <gtzahristas@hotmail.com>
2026-08-13 16:28:14 +0300

    August Big Update v5

--- eclipse_player/server/src/services/download.service.ts
+++ eclipse_player/server/src/services/download.service.ts
@@ -1,14 +1,13 @@
 import path from "path";
 import { ensurefileName } from "../guards/downloads.guard.js";
 
-const APKS = {
-    "1.2.8": "eclipse_player_v1.2.8.apk",
-    "1.2.9": "eclipse_player_v1.2.9.apk",
+const APKS = {    
+    "0.3.1": "eclipse_player_v0.3.1.apk",
 } as const;
 
 export const apkService = {
     getApkPath(version?: string): string {
-        const v = version ?? "1.2.8";
+        const v = version ?? "0.3.1";
         const fileName = APKS[v as keyof typeof APKS];
         ensurefileName(fileName);        
         return path.resolve("files", fileName);
